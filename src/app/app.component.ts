71d4730f2e9b3d62062fcf0969f82d1b6824a39f
Lior Bas <look551@gmail.com>
2025-06-02 20:37:35 +0300

    emit & Output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "./header/header.component";
-import { UserComponent } from "./user/user.component";
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
 
 @Component({
@@ -12,4 +12,8 @@ import { DUMMY_USERS } from './dummy-users';
 })
 export class AppComponent {
   users = DUMMY_USERS;
+
+  onSelectUser(id: string) {
+    console.log('Selected user with id ' + id);
+  }
 }
