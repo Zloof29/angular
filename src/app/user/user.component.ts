3dcec7871f0ec4b1e6c3617d91b7859ae65f88c0
Lior Bas <look551@gmail.com>
2025-06-03 20:50:43 +0300

    exercise

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -11,7 +11,12 @@ export class UserComponent {
   @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
-  @Output() select = new EventEmitter();
+  @Output() select = new EventEmitter<string>();
+
+  // id = input.required<string>();
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+  // select = output<string>();
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
